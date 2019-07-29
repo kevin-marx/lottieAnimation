c658958c9bc04e7ba9c685090e11be99dd2d83fa
Kevin M <kmarx@caltech.edu>
2019-07-29 09:51:13 -0700

    initial commit

---
+++ lottieAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  lottieAnimation
+//
+//  Created by Kevin Marx on 7/29/19.
+//  Copyright © 2019 Kevin Marx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
