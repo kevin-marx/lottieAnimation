95d8878dd88374dcc9881dfaca1f1404979e9e2c
Kevin M <kmarx@caltech.edu>
2019-07-29 10:40:29 -0700

    got the animation working

--- lottieAnimation/ViewController.swift
+++ lottieAnimation/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let animationView = AnimationView(name: "folderani")
+        
+        
+        animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 200)
+        animationView.center = self.view.center
+        animationView.contentMode = .scaleAspectFill
+        
+        animationView.loopMode = .autoReverse
+        
+        view.addSubview(animationView)
+        
+        animationView.play()
     }
 
 
